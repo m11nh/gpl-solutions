6956b2c6e932c911c3a44a2cf0ab82e4ff54d80c
minh <minh.nguyen@bukalapak.com>
2022-02-05 11:37:08 +1100

    completed activity

---
+++ 4.7/main.go
@@ -0,0 +1,32 @@
+// Rev reverses a slice.
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	//!+array
+	a := []byte("f的jk的   salfj")
+	reverse(a)
+	fmt.Println(string(a))
+}
+
+func rev(s []byte) {
+	N := len(s)
+	for i := 0; i < i/2; i++ {
+		end := N - 1 - i 
+		s[i], s[end] = s[end], s[i]
+	}
+}
+
+// reverse reverses a slice of bytes in place.
+func reverse(s []byte) {
+	for i := 0; i < len(s); {
+		_, size := utf8.DecodeRune(s[i:])
+		rev(s[i:i + size])
+		i+=size
+	}
+	rev(s[:])
+}
